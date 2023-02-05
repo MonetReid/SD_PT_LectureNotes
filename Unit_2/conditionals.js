2cebd08291a2e7770c07d036fd4c3bee5b346b06
Monet Reid <monetreid18@gmail.com>
2023-02-05 17:03:40 -0500

    Added Switch commands

--- Unit_2/conditionals.js
+++ Unit_2/conditionals.js
@@ -95,3 +95,89 @@ if (!rain) {
 } else {
     console.log("Get your jacket!");
 }
+
+// ? Switch Statement 
+ 
+/* 
+    keywords:
+        - switch
+        - case
+        - break
+            - takes us out of our code block
+        - default
+            - run if no cases match
+    Structure:
+    switch(expression) {
+        case ... :
+            return;
+            break;
+        case ... :
+            return;
+            break;
+        case ... :
+            return;
+            break;
+        default:
+            return;
+    }
+*/
+
+let officeCharacter = "Jim";
+
+switch (officeCharacter) {
+  case "Michael":
+    console.log("My mind is going a mile an hour.");
+    break;
+  case "Dwight":
+    console.log("Perfectenschlag");
+    break;
+  case "Jim":
+    console.log("Bears. Beets. Battlestar Galactica");
+    break;
+  case "Pam":
+    console.log("Yup");
+    break;
+  default:
+    console.log(`I'm sorry, ${officeCharacter}, but do I know you?`);
+}
+
+// String interpolation: using a variable in a string with backticks(`), dollar sign ($), and {}
+// interpolation definition: the insertion of something of a different nature into something else.
+
+let myName = "Kate";
+let bff = "Ben";
+console.log(`My name is ${myName} and my best friend is ${bff}!`);
+
+//let num = -7;
+//let num = 5;
+let num = 0;
+
+switch (true) {
+    case num < 0 && num > -10:
+        console.log("Case 1 ran");
+        break;
+    case num > 0: 
+        console.log("Case 2 ran");
+        break;
+    default:
+        console.log("Default ran, no case worked");
+        break;
+}
+
+
+let grade = "B";
+switch (true) {
+    case grade == "A":
+    case grade == "B":  // "A" & "B" don't have a break bc we want them to give the same action/message for either grade.
+        console.log("Great job.")
+        break;
+    case grade == "C":
+        console.log("Doing well, but maybe study a bit more.")
+        break;
+    case grade == "D": 
+    case grade == "F": 
+        console.log("Uh, I would really start studying...")
+        break;
+    default:
+        console.log("That's not a grade value...")
+}
