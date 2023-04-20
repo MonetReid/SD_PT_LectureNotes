b90fcf0f38c8c9befe5a55b95612d4a7f9ddc4d7
Monet Reid <monetreid18@gmail.com>
2023-04-20 18:49:19 -0400

    a lot of notes

--- Unit_6/Movies_MongoDB/app.js
+++ Unit_6/Movies_MongoDB/app.js
@@ -10,6 +10,8 @@ const log = console.log;
 // ----------------------- Controllers:----------------------
 const users = require("./controllers/users.controller");
 
+const movies = require("./controllers/movie.controller");
+
 // Require in the mongoose middleware, pulled/used from node_modules
 const mongoose = require("mongoose");
 // Create a variable for our connection address variable from the .env
@@ -40,5 +42,7 @@ app.use(express.json());
 //------------------- Routes to Controllers --------------------------
 // http://localhost:4000/user
 app.use("/user", users);
+// http://localhost:4000/movies
+app.use("/movies", movies);
 
 app.listen(PORT, () => log(`Movie Server on Port: ${PORT}`));
